632d39ae8897e9bf73e09018f65e5a12e832cb94
Rane <mashtarov.wf@mail.ru>
2024-04-19 18:26:22 +0700

    Refactoring AdminPanel

--- src/controller/Products.php
+++ src/controller/Products.php
@@ -1,46 +1,61 @@
 <?
 namespace controller;
+
 use servises\Connect;
 
 class Products{
 
     public function AddProduct($data){
+        print_r($data);
         $productName = $data['productName'];
         $category = $data['category'] == 'Мужское' ? 1 : 2;
         $brand = $data['brand'];
+        $size = $data['size'];
         $quantity = $data['quantity'];
         $price = $data['price'];
         $targetFile = null;
         if($_FILES["product-photo"]['size'] != 0){
             $targetFile = $this->UpLoadPhoto(); 
         }
-        if(!empty($data['ProductID'])){
-            $productID = $data['ProductID'];
+        if(!empty($data['VariantID'])){
+            $VariantID = $data['VariantID'];
+            $productID = mysqli_fetch_assoc(Connect::$connect->query("SELECT ProductID FROM ProductVariants WHERE VariantID = $VariantID"))["ProductID"];
             if($targetFile != null){
-                Connect::$connect->query("UPDATE Products SET ProductName = '$productName', CategoryID = '$category', ProductBrand = '$brand', Price = '$price', Stock = '$quantity', ImageURL = '$targetFile' WHERE ProductID = '$productID'");
+                Connect::$connect->query("UPDATE ProductVariants SET Size = '$size', Price = '$price', Stock = '$quantity' WHERE VariantID = '$VariantID'");
+                Connect::$connect->query("UPDATE Products SET ProductName = '$productName', CategoryID = '$category', ProductBrand = '$brand',  ImageURL = '$targetFile' WHERE ProductID = '$productID'");
             }
             else{
-                Connect::$connect->query("UPDATE Products SET ProductName = '$productName', CategoryID = '$category', ProductBrand = '$brand', Price = '$price', Stock = '$quantity' WHERE ProductID = '$productID'");
+                Connect::$connect->query("UPDATE ProductVariants SET Size = '$size', Price = '$price', Stock = '$quantity' WHERE VariantID = '$VariantID'");
+                Connect::$connect->query("UPDATE Products SET ProductName = '$productName', CategoryID = '$category', ProductBrand = '$brand' WHERE ProductID = '$productID'");
             }
         }
         else{
-            if (!(empty($productName) || empty($category) || empty($brand) || empty($quantity) || empty($price))) {
-                Connect::$connect->query("INSERT INTO Products (ProductName, CategoryID, ProductBrand, Price, Stock, ImageURL) 
-                VALUES ('$productName', '$category', '$brand', '$price', '$quantity', '$targetFile')");
+            if (!(empty($productName) || empty($category) || empty($brand) || empty($quantity) || empty($price) || empty($size))) {
+                if(mysqli_num_rows(Connect::$connect->query("SELECT * FROM Products WHERE ImageURL = '$targetFile'")) == 0){
+                    Connect::$connect->query("INSERT INTO Products (ProductName, CategoryID, ProductBrand, ImageURL) VALUES ('$productName', '$category', '$brand', '$targetFile')");
+                    $productId = mysqli_fetch_assoc(Connect::$connect->query("SELECT * FROM Products WHERE ImageURL = '$targetFile'"))['ProductID'];
+                    Connect::$connect->query("INSERT INTO ProductVariants (ProductID, Size, Price, Stock) VALUES ('$productId', '$size', '$price', '$quantity')");
+                }
+                else{
+                    $productId = mysqli_fetch_assoc(Connect::$connect->query("SELECT * FROM Products WHERE ImageURL = '$targetFile'"))['ProductID'];
+                    Connect::$connect->query("INSERT INTO ProductVariants (ProductID, Size, Price, Stock) VALUES ('$productId', '$size', '$price', '$quantity')");
+                }
             }
         }
         header("Location: /admin");
     }
 
     public function DeleteProduct($data){
-        $productID = $data['ProductID'];
+        $VariantID = $data['VariantID'];
+        $productID = mysqli_fetch_assoc(Connect::$connect->query("SELECT ProductID FROM ProductVariants WHERE VariantID = $VariantID"))["ProductID"];
+        Connect::$connect->query("DELETE FROM ProductVariants WHERE VariantID = $VariantID");
         Connect::$connect->query("DELETE FROM Products WHERE ProductID = $productID");
         header("Location: /admin");
     }
 
     
     private function UpLoadPhoto(){
-        $targetDir = "images/";
+        $targetDir = "./images/";
         $targetFile = $targetDir . basename($_FILES["product-photo"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
@@ -57,7 +72,7 @@ class Products{
         if (file_exists($targetFile)) {
             return $targetFile;
         }
-        if ($_FILES["product-photo"]["size"] > 500000) {
+        if ($_FILES["product-photo"]["size"] > 50000000) {
             $uploadOk = 0;
         }
         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
--- view/pages/admin/admin.php
+++ view/pages/admin/admin.php
@@ -1,3 +1,9 @@
+<?
+		use servises\Connect;
+	// if($_SESSION['role'] < 49) header("Location: /"); //check user role
+	$ProductVariants = mysqli_fetch_all(Connect::$connect->query("SELECT * FROM ProductVariants"), MYSQLI_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -12,9 +18,6 @@
 	</head>
 	<body>
 		<? include './view/partials/header.php'?>
-		<?
-		if($_SESSION['role'] < 49) header("Location: /");
-		?>
 		<main class="main">
 			<div class="container">
 				<h2 class="main__header">Панель администратора</h2>
@@ -35,6 +38,9 @@
 								<td class="brand">
 									Бренд
 								</td>
+								<td class="size">
+									Размер
+								</td>
 								<td class="quantity">
 									Кол-во
 								</td>
@@ -50,62 +56,83 @@
 							</tr>
 						<tbody>
 							<?php
-							use servises\Connect;
-							$data = mysqli_fetch_all(Connect::$connect->query("SELECT * FROM Products"), MYSQLI_ASSOC);
-							foreach($data as $item): ?>
+							foreach($ProductVariants as $item): 
+								$productId = $item['ProductID'];
+								$product = mysqli_fetch_assoc(Connect::$connect->query("SELECT * FROM Products WHERE ProductID = $productId"))
+							?>
+							
 							<tr>
-								<td><img src="<?= $item['ImageURL']?>" alt="<?= $item['ImageURL']?>" style="width: 80px; height 80px;"></td>
-								<td><?= $item['ProductName']?></td>
-								<td><?= $item['CategoryID'] == 1 ? 'Мужское' : 'Женское' ?></td>
-								<td><?= $item['ProductBrand']?></td>
+								<td><img src="<?= $product['ImageURL']?>" alt="<?= $product['ImageURL']?>" style="width: 80px; height 80px;"></td>
+								<td><?= $product['ProductName']?></td>
+								<td><?= $product['CategoryID'] == 1 ? 'Мужское' : 'Женское' ?></td>
+								<td><?= $product['ProductBrand']?></td>
+								<td><?= $item['Size']?></td>
 								<td><?= $item['Price']?></td>
 								<td><?= $item['Stock']?></td>
 								<td>
 								<form action="/deleteProduct" method="POST">
-									<input type="hidden" name="ProductID" value="<?= $item['ProductID'] ?>">
+									<input type="hidden" name="VariantID" value="<?= $item['VariantID'] ?>">
 									<button type="submit" class="delete-button">Удалить</button>
 								</form>
 								</td>
-								<td><a href="/admin?ProductID=<?=$item['ProductID']?>">Изменить</a></td>
+								<td><a href="/admin?VariantID=<?=$item['VariantID']?>">Изменить</a></td>
 							</tr>
 							<?php endforeach; ?>
 						</tbody>
 						</table>
 					</div>
-					<button class="save-button button">Количество товаров: <?= count($data)?></button>
+					<button class="save-button button">Количество товаров: <?= count($ProductVariants)?></button>
 				</div>
 				<div class="form-wrapper">
 					<?
-						$ProductID = isset($_GET['ProductID']) ? $_GET['ProductID'] : '';
+						$VariantID = isset($_GET['VariantID']) ? $_GET['VariantID'] : '';
 						$productCategory = false;
-						if($ProductID !== ''){
-							$product = mysqli_fetch_assoc(Connect::$connect->query("SELECT * FROM Products WHERE ProductID = $ProductID"));
-							$productCategory = $product['CategoryID'] == 1 ? 'Мужское' : 'Женское'; 
+						$productSize = null;
+						if($VariantID !== ''){
+
+							$ProductVariant = mysqli_fetch_assoc(Connect::$connect->query("SELECT * FROM ProductVariants WHERE VariantID = $VariantID"));
+							$ProductId = $ProductVariant['ProductID'];
+							$product = mysqli_fetch_assoc(Connect::$connect->query("SELECT * FROM Products WHERE ProductID = $ProductId"));
+							$product = array_merge($ProductVariant, $product);
+
+
+							$productCategory = $product['CategoryID'] == 1 ? 'Мужское' : 'Женское';
+							$productSize = $product['Size'];
 						}
 					?>
 					<form class="panel-form" method="post" action="/addProduct" enctype="multipart/form-data">
-					<input type="hidden" name="ProductID" value="<?= $_GET['ProductID'] ?? ''?>">
+					<input type="hidden" name="VariantID" value="<?= $_GET['VariantID'] ?? ''?>">
 					<label for="brand">Бренд</label>
-					<input type="text" id="brand" name="brand" class="product-brand" value="<?= isset($_GET['ProductID']) ?  $product['ProductBrand'] : ''?>" required>
+					<input type="text" id="brand" name="brand" class="product-brand" value="<?= isset($_GET['VariantID']) ?  $product['ProductBrand'] : ''?>" required>
 
 					<label for="name">Название товара</label>
-					<input type="text" id="name" name="productName" class="product-name" value="<?= isset($_GET['ProductID']) ?  $product['ProductName'] : ''?>" required>
+					<input type="text" id="name" name="productName" class="product-name" value="<?= isset($_GET['VariantID']) ?  $product['ProductName'] : ''?>" required>
 					
 					<label for="category">Категория</label>
 					<select id="category" name="category" class="product-category" required>
 						<option value="Мужское" <?= $productCategory == 'Мужское' ? 'selected' : '' ?>>Мужское</option>
 						<option value="Женское" <?= $productCategory == 'Женское' ? 'selected' : '' ?>>Женское</option>
 					</select>
+
+					<label for="size">Размер</label>
+					<select id="size" name="size" class="product-size" required>
+						<option value="XS" <?= $productSize == 'XS' ? 'selected' : '' ?>>XS</option>
+						<option value="S" <?= $productSize == 'S' ? 'selected' : '' ?>>S</option>
+						<option value="M" <?= $productSize == 'M' ? 'selected' : '' ?>>M</option>
+						<option value="L" <?= $productSize == 'L' ? 'selected' : '' ?>>L</option>
+						<option value="XL" <?= $productSize == 'XL' ? 'selected' : '' ?>>XL</option>
+						<option value="XXL" <?= $productSize == 'XXL' ? 'selected' : '' ?>>XXL</option>
+					</select>
 					
 					<label for="quantity">Количество</label>
-					<input type="number" id="quantity" name="quantity" class="product-quantity" value="<?= isset($_GET['ProductID']) ?  $product['Stock'] : ''?>" required>
+					<input type="number" id="quantity" name="quantity" class="product-quantity" value="<?= isset($_GET['VariantID']) ?  $product['Stock'] : ''?>" required>
 					
 					<label for="price">Цена</label>
-					<input type="text" id="price" name="price" class="product-price" value="<?= isset($_GET['ProductID']) ?  $product['Price'] : ''?>" required>
+					<input type="text" id="price" name="price" class="product-price" value="<?= isset($_GET['VariantID']) ?  $product['Price'] : ''?>" required>
 					
 					<input type="file" id="photo-uploaded" name="product-photo">
 					
-					<button class="form__add-button button" type="submit"><?= isset($_GET['ProductID']) ? 'Сохранить изменения' : 'Добавить товар'?></button>
+					<button class="form__add-button button" type="submit"><?= isset($_GET['VariantID']) ? 'Сохранить изменения' : 'Добавить товар'?></button>
 					</form>
 			</div>
 		</div>
